240663c935caba97ee3b67f3da3d5b7575ab620d
Janani M <141489026+MJ-Janani@users.noreply.github.com>
2024-06-26 10:45:37 +0530

    Update xor_bitwise.v

--- XOR_bitwise/xor_bitwise.v
+++ XOR_bitwise/xor_bitwise.v
@@ -3,12 +3,12 @@ parameter N=16;//16 bit
 input [N-1:0]a;
 input [N-1:0]b;
 output [N-1:0]out;
-genvar p;
+genvar p;//generate variable
 
 generate 
-for (p=0;p<N;p=p+1)
+  for (p=0;p<N;p=p+1)//16 copies of xor are generated via loop
 
-begin : xorbit
+begin : xorbit//begin : end
 xor XG(out[p],a[p],b[p]);
 end
 endgenerate
