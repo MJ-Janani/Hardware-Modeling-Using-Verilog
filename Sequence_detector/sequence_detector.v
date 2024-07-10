2aabb13cc357469088e1f6155574404ad648b7bf
Janani M <141489026+MJ-Janani@users.noreply.github.com>
2024-07-10 19:16:43 +0530

    Update sequence_detector.v

--- Sequence_detector/sequence_detector.v
+++ Sequence_detector/sequence_detector.v
@@ -26,26 +26,7 @@ module sequence_detector(x,clk,rst,z);
             end
         endcase 
 endmodule
-module sequence_tb;
-    reg x,clk,rst;
-    wire z;
-    //reg[1:0]PS,NS;
-    sequence_detector uut(x,clk,rst,z);
-    initial begin
-        $dumpfile("sequence_detector.vcd");
-        $dumpvars(0,sequence_tb);
-        rst=1'b1;clk=1'b0;
-        #10 rst=1'b0;
-    end
-    always #5 clk=~clk;
-    initial begin
-        #8 x=0; #10 x=0 ; #10 x=1;#10 x=1;
-        #10 x=0; #10 x=1 ; #10 x=1;#10 x=0;
-        #10 x=1; #10 x=0 ; #10 x=1;#10 x=1;
-        #10 x=1; #10 x=0 ; #10 x=0;#10 x=1;
-        #10 $finish();
-    end
-endmodule
+
     
             
             
